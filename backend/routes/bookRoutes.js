4e32b600c020ed7aa09df92ca4a8192bbc613a79
shieldstring <edmunddaniels06@gmail.com>
2025-03-20 16:43:23 +0100

    Handle User Roles and Permissions

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const { protect } = require('../middleware/authMiddleware');
+const { protect, admin } = require('../middleware/authMiddleware');
 const { getBooks, addBook, editBook, deleteBook, trackPurchase, addReview, getBookById, getRecommendations, searchBooks } = require('../controllers/bookController');
 
 const router = express.Router();
 
 router.get('/', getBooks);
 router.get('/:id', getBookById);
-router.get('/recommendations', protect, getRecommendations); // Get AI-curated recommendations
-router.get('/search', searchBooks); // Search books manually
-router.post('/', protect, addBook);
-router.put('/:id', protect, editBook);
-router.delete('/:id', protect, deleteBook);
+router.get('/recommendations', protect, getRecommendations);
+router.get('/search', searchBooks);
+router.post('/', protect, admin, addBook); // Only admins can add books
+router.put('/:id', protect, admin, editBook); // Only admins can edit books
+router.delete('/:id', protect, admin, deleteBook); // Only admins can delete books
 router.post('/:id/purchase', protect, trackPurchase);
 router.post('/:id/reviews', protect, addReview);
 
