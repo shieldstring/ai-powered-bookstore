b92a0e0d45819c65c51cb9a96f7eb1c07bdc96ad
shieldstring <edmunddaniels06@gmail.com>
2025-07-20 18:55:02 +0100

    fixed findSellerByIdOrSlug 2

--- utils/findSellerByIdOrSlug.js
+++ utils/findSellerByIdOrSlug.js
@@ -3,21 +3,31 @@ const User = require("../models/User");
 const Seller = require("../models/Seller");
 
 const findSellerByIdOrSlug = async (identifier) => {
-  let userQuery = { role: "seller" };
+  let user = null;
+  let sellerProfile = null;
 
   if (mongoose.Types.ObjectId.isValid(identifier)) {
-    userQuery._id = identifier;
+    user = await User.findById(identifier).select("name email role slug");
+    if (user && user.role === "seller") {
+      sellerProfile = await Seller.findOne({ user: user._id });
+      return { user, sellerProfile };
+    }
   } else {
-    userQuery.slug = identifier; // Ensure User model has this field!
+    // Try User.slug first
+    user = await User.findOne({ slug: identifier }).select("name email role slug");
+    if (user && user.role === "seller") {
+      sellerProfile = await Seller.findOne({ user: user._id });
+      return { user, sellerProfile };
+    }
+
+    // If not found in User, check Seller slug
+    sellerProfile = await Seller.findOne({ slug: identifier }).populate("user", "name email role slug");
+    if (sellerProfile && sellerProfile.user.role === "seller") {
+      return { user: sellerProfile.user, sellerProfile };
+    }
   }
 
-  const user = await User.findOne(userQuery).select("name email slug role");
-  if (!user) return null;
-
-  const sellerProfile = await Seller.findOne({ user: user._id });
-  if (!sellerProfile) return null;
-
-  return { user, sellerProfile };
+  return null; // Not found
 };
 
 module.exports = findSellerByIdOrSlug;
