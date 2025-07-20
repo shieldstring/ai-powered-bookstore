c2028fdd3bbe8c95c7c21e654e77884f228a6650
shieldstring <edmunddaniels06@gmail.com>
2025-07-20 02:24:25 +0100

    fixed get front req 2

--- utils/findSellerByIdOrSlug.js
+++ utils/findSellerByIdOrSlug.js
@@ -2,11 +2,27 @@ const mongoose = require("mongoose");
 const Seller = require("../models/Seller");
 
 const findSellerByIdOrSlug = async (identifier) => {
-  const isObjectId = mongoose.Types.ObjectId.isValid(identifier);
+  if (mongoose.Types.ObjectId.isValid(identifier)) {
+    // Try as Seller ID first
+    let seller = await Seller.findById(identifier).populate(
+      "user",
+      "email name"
+    );
+    if (seller) return seller;
 
-  const query = isObjectId ? { _id: identifier } : { slug: identifier };
+    // Then try as User ID
+    seller = await Seller.findOne({ user: identifier }).populate(
+      "user",
+      "email name"
+    );
+    if (seller) return seller;
+  }
 
-  return await Seller.findOne(query).populate("user", "email name");
+  // Try as slug last
+  return await Seller.findOne({ slug: identifier }).populate(
+    "user",
+    "email name"
+  );
 };
 
 module.exports = findSellerByIdOrSlug;
